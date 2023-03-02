33320ccfef411ab8f8b87c6d68918b137c2c750f
gonzapena <117782732+gonzapena@users.noreply.github.com>
2023-03-02 22:11:04 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -1,44 +1,54 @@
 
-// calculadora de propinas
-// object
-class Table {
-    constructor(nombre,apellido,edad){
-        this.amount = nombre;
-        this.n_people_pay = apellido;
-        this.tip = edad
+
+ // Agregamos un event listener al botón para que llame a la función "calculateTip" cuando se haga click en él
+ document.getElementById('calculate-btn').addEventListener('click', calculateTip);
+
+ function calculateTip() {
+   const amount = parseInt(document.getElementById('amount').value);
+   const n_people_pay = parseInt(document.getElementById('people').value);
+   const tip = parseInt(document.getElementById('tip').value);
+   const bill_details= new Table(amount ,n_people_pay , tip);
+   table_tips.push(bill_details);
+   localStorage.setItem('table_tips', JSON.stringify(table_tips)); // Guardamos la tabla de propinas en localStorage
+
+   function DivideBill (amount,n_people_pay,tip){
+     let amount_tip = (amount * tip) / 100;
+     let bill_tip = amount + amount_tip ;
+     let total_per_person = bill_tip / n_people_pay;
+     return total_per_person
+   }
+
+   if( amount > 0){
+     const totalAmount = (((amount * tip) / 100 + amount));
+     const totalPerPerson = DivideBill(amount, n_people_pay,tip);
+     const bill = table_tips.find(table => table.amount === amount);
+     let billDetails = '';
+     if (bill) {
+       billDetails = `Dato total de la cuenta: Monto ${bill.amount}`;
+     } else {
+       billDetails = "No se encontró la cuenta.";
+     }
+     document.getElementById('total').innerHTML = `El total a pagar incluyendo propina es de: ${totalAmount}`;
+     document.getElementById('perPerson').innerHTML = `El total a pagar por persona es de: ${totalPerPerson}`;
+     document.getElementById('billDetails').innerHTML = billDetails;
+     // Workshop - agregar resultados
+     let workshopResults = document.getElementById('workshop-results');
+     let p = document.createElement('p');
+     let text = document.createTextNode(`Monto total: ${amount}, Número de personas: ${n_people_pay}, Porcentaje de propina: ${tip}`);
+     p.appendChild(text);
+     workshopResults.appendChild(p);
     }
-}
-
-// array
-let table_tips = [];
-
-for(let table_data of table_tips  ){
-   
-    amount = parseInt( prompt('Cuánto es el total que se debe pagar?: '));
-    n_people_pay = parseInt( prompt ('¿Cuántas personas van a pagar?: '));
-    tip = parseInt(prompt ('¿Cuál es el porcentaje de propina que se va a pagar?: '));
-    bill_details= new Table(amount ,n_people_pay , tip);
-
-    table_tips.push(new_customer)
-
-    function DivideBill (amount,n_people_pay,tip){ 
-        console.log(amount,n_people_pay)
-        let amount_tip = (amount * tip) / 100;
-        let bill_tip = amount + amount_tip ;
-        let total_per_person = bill_tip / n_people_pay;
-
-        return total_per_person
-      }
-
-    if( amount > 0){
-        alert("El total a pagar incluyendo propina es de:" + (((amount * tip) / 100 + amount)));  
-        alert("El total a pagar por persona es de: " + DivideBill(amount, n_people_pay,tip) ); 
-        alert("Dato total de la cuenta: Monto " + table_tips.amount , "personas" + table_tips.amount , "tip a pagar" + table_tips.tip + "%"); 
-    }
-
     else if( amount <= 0){
-        alert("No valido");
-        break;
+      alert("No valido");
     }
+ }
 
-}
+ class Table {
+   constructor(amount,people,tip){
+     this.amount = amount;
+     this.n_people_pay = people;
+     this.tip = tip
+   }
+ }
+
+ let table_tips = JSON.parse(localStorage.getItem('table_tips')) || []; // Obtenemos la tabla de propinas de localStorage o la inicializamos como un array vacío si no existe
